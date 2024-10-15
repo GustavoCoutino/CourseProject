6459bbf81262cd472711ac1cacb5358bef7d28fd
Gustavo Coutiño <gcoutino04@gmail.com>
2024-10-14 19:37:03 -0600

    Changed request

--- client/src/services/authService.js
+++ client/src/services/authService.js
@@ -2,7 +2,7 @@ import apiClient from "./apiClient";
 
 async function login(email, password) {
   try {
-    const response = await apiClient.post("/login", { email, password });
+    const response = await apiClient.post("/auth/login", { email, password });
     return response.data;
   } catch (error) {
     console.error(error);
